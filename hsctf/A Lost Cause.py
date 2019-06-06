4a32934c2052d97e43b43dbdd48faee399776bde
Mark Bedaywi <50279151+Supermac30@users.noreply.github.com>
2019-06-06 16:04:40 -0400

    Delete A Lost Cause.py

--- hsctf/A Lost Cause.py
+++
@@ -1,9 +0,0 @@
-def shift(letter, amount):
-    return chr(ord('A')+((ord(letter)-amount-ord("A"))%26))
-ciphertext = "CGULKVIPFRGDOOCSJTRRVMORCQDZG"
-for i in range(27):
-    fixed = ""
-    for j in range(len(ciphertext)):
-        fixed += shift(ciphertext[j],i-j)
-    print(fixed)
-    input()
